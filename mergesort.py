81cdd37e934c40e5b3f1f5413e4b87358356760b
Jarrad Billiot <61070441+EachFlounder967@users.noreply.github.com>
2026-03-27 06:49:29 -0500

    Create mergesort.py

---
+++ mergesort.py
@@ -0,0 +1,34 @@
+def merge_sort(nums):
+    if len(nums) < 2:
+        return nums
+    mid = len(nums) // 2
+    left = nums[:mid]
+    right = nums[mid:]
+    sorted_left_side = merge_sort(left)
+    sorted_right_side = merge_sort(right)
+    return merge(sorted_left_side, sorted_right_side)
+    
+
+
+def merge(first, second):
+    final = []
+    i = 0
+    j = 0
+
+    while i < len(first) and j < len(second):
+        if first[i] <= second[j]:
+            final.append(first[i])
+            i += 1
+        else:
+            final.append(second[j])
+            j += 1
+
+    while i < len(first):
+        final.append(first[i])
+        i += 1
+
+    while j < len(second):
+        final.append(second[j])
+        j += 1
+    
+    return final
